c23057715e7cef7d62da9adff321b56e56e4b0bc
Zainab <138154768+Zeey76@users.noreply.github.com>
2024-12-22 22:55:27 +0100

    Update java.js

--- java.js
+++ java.js
@@ -7,7 +7,7 @@ async function fetchData() {
     commentsData = await response.json();
     commentMessages(commentsData);
     updateTime();
-    setInterval(updateTime, 0);
+    setInterval(updateTime, 1000);
   } catch (error) {
     console.error("Error fetching data");
   }
